dc0d98fdec0159649045694928d443dbaa8379b0
anna.shkulova <anna.shkulova@thomsonreuters.com>
2019-11-07 14:27:37 +0200

    Add allure reporting

--- src/test/java/com/mentoring/ui/suites/RegressionSuite.java
+++ src/test/java/com/mentoring/ui/suites/RegressionSuite.java
@@ -1,7 +1,5 @@
 package com.mentoring.ui.suites;
 
-import com.mentoring.ui.gmail.MessageTest;
-import com.mentoring.ui.google.search.SearchTest;
 import com.mentoring.ui.kieskeurig.FiltersTest;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
@@ -9,7 +7,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
 @SelectClasses({FiltersTest.class
-                , MessageTest.class
-                , SearchTest.class})
+                })
 public class RegressionSuite {
 }
